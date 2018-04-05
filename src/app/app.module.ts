2f6116c1590cc80ec7b495efe8e5983302e9cc87
696969 <tinkertechinc@gmail.com>
2018-04-05 14:59:17 +0800

    added http module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpModule } from "@angular/http";
 
 
 import { MyApp } from './app.component';
@@ -17,6 +18,7 @@ import { TodoService } from "../providers/todo-service";
     ArchivedTodosPage
   ],
   imports: [
+    HttpModule,
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
